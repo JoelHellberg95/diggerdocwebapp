c42826c7b91c500176b6a85e3f89963cb11694e0
Joel Hellberg <118288883+JoelHellberg95@users.noreply.github.com>
2024-05-15 18:12:18 +0200

    Initial commit

---
+++ diggerdocwebapp/src/app/page.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <main>
+      <h1>Welcome to DiggerDoc!</h1>
+      <p>
+        DiggerDoc is a web application that allows you to manage your service jobs and invoices.
+      </p>
+    </main>
+  );
+}
